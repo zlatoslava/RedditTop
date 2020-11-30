47ad749886d38402939136f4cf1c02214fca3ee8
zlatoslava <anckamur@gmail.com>
2020-11-30 12:22:37 +0200

    Create model classes for API response

---
+++ app/src/main/java/com/example/reddittop/data/model/Models.kt
@@ -0,0 +1,31 @@
+package com.example.reddittop.data.model
+
+import com.google.gson.annotations.SerializedName
+
+data class RedditApiResponse (
+    val data: RedditData
+)
+
+data class RedditData (
+    val children: List<RedditPostContainer>, val after: String?, val before: String?
+)
+
+data class RedditPostContainer (
+    val data: RedditPost
+)
+
+data class RedditPost (
+    @SerializedName("id")
+    val id: String,
+    @SerializedName("author")
+    val author: String,
+    @SerializedName("title")
+    val title: String,
+    @SerializedName("created_utc")
+    val date: Long,
+    @SerializedName("thumbnail")
+    val thumbnailUrl: String,
+    @SerializedName("num_comments")
+    val commentCount: Int
+)
+
