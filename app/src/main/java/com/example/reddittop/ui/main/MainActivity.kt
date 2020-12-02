79db36c74edcfb3327772fe800e02140a3fc06fd
zlatoslava <anckamur@gmail.com>
2020-12-02 12:15:00 +0200

    Add an up button in the appbar

--- app/src/main/java/com/example/reddittop/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/reddittop/ui/main/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.reddittop.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.reddittop.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val controller = this.findNavController(R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this, controller)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val controller = this.findNavController(R.id.nav_host_fragment)
+        return controller.navigateUp()
     }
 }
